c83123168e888fa9ab3ad72458be47a7202ea221
rlaehgns5399 <rlaehgns5398@gmail.com>
2019-01-14 16:29:11 +0900

    Implement: UserDAO

--- src/io/github/rlaehgns5399/UserDAO.java
+++ src/io/github/rlaehgns5399/UserDAO.java
@@ -1,7 +1,42 @@
 package io.github.rlaehgns5399;
 
+import java.sql.*;
+
 /**
  * Created by KimDoHoon on 2019-01-08 008.
  */
 public class UserDAO {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+
+        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword((rs.getString("password")));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
 }
