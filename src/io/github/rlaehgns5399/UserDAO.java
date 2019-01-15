95baf6ead77497dc865931b878052bd2c4d0939e
rlaehgns5399 <rlaehgns5398@gmail.com>
2019-01-15 16:38:05 +0900

    Modify wrong code in UserDAO, Add: test code at main

--- src/io/github/rlaehgns5399/UserDAO.java
+++ src/io/github/rlaehgns5399/UserDAO.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class UserDAO {
     public void add(User user) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3307/springbook?useSSL=false", "root", "root");
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
         ps.setString(1, user.getId());
@@ -22,10 +22,11 @@ public class UserDAO {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.jdbc.driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3307/springbook?useSSL=false", "root", "root");
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
         ResultSet rs = ps.executeQuery();
         rs.next();
         User user = new User();
