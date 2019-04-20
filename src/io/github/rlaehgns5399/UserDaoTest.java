8cbec981582abf00bb88e3f18a170f6f6bd309f2
rlaehgns5399 <rlaehgns5398@gmail.com>
2019-04-20 17:23:05 +0900

    Make UserDaoTest

---
+++ src/io/github/rlaehgns5399/UserDaoTest.java
@@ -0,0 +1,26 @@
+package io.github.rlaehgns5399;
+
+import java.sql.SQLException;
+
+/**
+ * Created by KimDoHoon on 2019-04-20 020.
+ */
+public class UserDaoTest {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+        UserDAO dao = new DaoFactory().userDAO();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("김도훈");
+        user.setPassword("root");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공!");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + "조회 성공!");
+    }
+}
